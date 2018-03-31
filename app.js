ec733a736cba77d6328e36d9ea185b8f99c889fa
Yong Lei <will19ie95@gmail.com>
2018-03-30 22:26:56 -0400

    added cors support

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 // var session = require("express-session");
+var cors = require('cors');
 var passport = require('./passport');
 var db = require('./db');
 const secret = require("./secret");
@@ -22,6 +23,7 @@ app.set('view engine', 'hbs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
